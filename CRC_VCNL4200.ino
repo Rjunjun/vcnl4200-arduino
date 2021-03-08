9b411946a7e56544d853729051724d10342accdd
Rjunjun <tristana1118@gmail.com>
2021-03-09 01:42:15 +0800

    init

---
+++ CRC_VCNL4200.ino
@@ -0,0 +1,50 @@
+/*
+ Name:    CRC_VCNL4200.ino
+ Created: 8/27/2017 4:10:37 PM
+ Author:  jlaing
+*/
+#include <arduino-timer.h>
+#include "CRC_VCNL4200.h"
+#define led_pin 5
+
+CRC_VCNL4200 vcnl4200;
+int pinReading = 0;
+uint16_t threshold_appxmt = 150;
+uint16_t current_appxmt;
+auto timer = timer_create_default(); // create a timer with default settings
+
+bool vcnl_timer(void *) {
+  current_appxmt = vcnl4200.getProximity();
+//  Serial.print("Proximity: ");
+  Serial.println(current_appxmt);
+//  Serial.print("Ambient: ");
+//  Serial.println(vcnl4200.getAmbient());
+  if(current_appxmt > threshold_appxmt){
+    digitalWrite(led_pin, 0);
+    digitalWrite(13, 0);
+  }
+  else{
+    digitalWrite(led_pin, 1);
+    digitalWrite(13, 1);
+  }
+  return true;
+}
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Booting.");
+  if (vcnl4200.exists()) {
+    Serial.println("VCNL4200 found");
+    vcnl4200.initialize();
+    Serial.println("VCNL4200 initialized");
+  }
+  pinMode(7, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(led_pin, OUTPUT);
+  timer.every(1, vcnl_timer);
+}
+
+void loop() {
+  timer.tick();
+}
---
+++ seriel_read.py
@@ -0,0 +1,32 @@
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+ser = serial.Serial("COM8", 9600)
+x_c = 1000
+pxmt_list = [0] * x_c
+
+for i in range(4) :
+    str(ser.readline())[2:][:-5]
+fig, ax = plt.subplots()
+plt.axis([0, x_c, 0, 65535])
+plt.title("Dynamic Plot of vcnl4200",fontsize=25)
+plt.xlabel("ms",fontsize=16)
+plt.ylabel("Proximity",fontsize=16)
+line, = ax.plot(pxmt_list)
+
+def animate(i):
+    cc=str(ser.readline())[2:][:-5]
+    pxmt_list[i % x_c] = int(cc)
+    if(i % x_c == 0) :
+        for i in range(x_c):
+            pxmt_list[i] = int(cc)
+    line.set_ydata(pxmt_list)  # update the data.
+    return line,
+
+ani = animation.FuncAnimation(
+    fig, animate, interval=1, blit=True, save_count=50)
+
+plt.show()
+
