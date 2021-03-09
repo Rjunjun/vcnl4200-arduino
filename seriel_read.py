bcaab4cc7c79d0c069246babf769f0493163492f
Rjunjun <tristana1118@gmail.com>
2021-03-09 13:59:55 +0800

    update animate plot

--- seriel_read.py
+++ seriel_read.py
@@ -2,31 +2,29 @@ import serial
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from collections import deque
 
 ser = serial.Serial("COM8", 9600)
 x_c = 1000
-pxmt_list = [0] * x_c
+pxmt_list = deque([0] * x_c)
 
-for i in range(4) :
-    str(ser.readline())[2:][:-5]
 fig, ax = plt.subplots()
 plt.axis([0, x_c, 0, 65537])
 plt.title("Dynamic Plot of vcnl4200",fontsize=25)
 plt.xlabel("ms",fontsize=16)
 plt.ylabel("Proximity",fontsize=16)
 line, = ax.plot(pxmt_list)
-
-def animate(i):
-    cc=str(ser.readline())[2:][:-5]
-    pxmt_list[i % x_c] = int(cc)
-    if(i % x_c == 0) :
-        for i in range(x_c):
-            pxmt_list[i] = 0
-    line.set_ydata(pxmt_list)  # update the data.
+for i in range(4) :
+    print(str(ser.readline())[2:][:-5])
+    
+def animate(i, data):
+    data[-1] = int(str(ser.readline())[2:][:-5])
+    data.rotate(1)
+    line.set_ydata(data)  # update the data.
     return line,
 
 ani = animation.FuncAnimation(
-    fig, animate, interval=1, blit=True, save_count=50)
+    fig, animate, fargs=[pxmt_list], interval=1, blit=True, save_count=50, frames=x_c)
 
 plt.show()
 
